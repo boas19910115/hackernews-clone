0d1cbd0791cf01942b8fca23a22876e2f0803afa
akash <akash619@protonmail.com>
2019-06-07 22:35:42 +0530

    test2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import Header from './Header';
 import Stories from './Fetch-TopStories';
 import Table from './Table';
 
-<<<<<<< HEAD
-ReactDOM.render(<Header/>, document.getElementById('root'));
-=======
 ReactDOM.render(
   <div>
     <Header />
     <Stories />
   </div>
   , document.getElementById('root'));
->>>>>>> 527d648e5c783729e92384247d97bb9fc60d9de0
