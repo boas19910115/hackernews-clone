54644ffd30b5b50854ca871f7f2c00af2da9bb6f
Naman Modi <modi.naman14@gmail.com>
2019-06-08 02:26:20 +0530

    Improved Data Displaying & Error Handling for missing URLs in API

--- src/Fetch-TopStories.js
+++ src/Fetch-TopStories.js
@@ -4,10 +4,6 @@ import './Stories.css'
 var storiesArr = [];
 var mutableStoriesArr = [];
 
-function jsonToMap(jsonStr) {
-  return new Map(JSON.parse(jsonStr));
-}
-
 class Stories extends Component {
   // constructor()
   state = {
@@ -42,16 +38,18 @@ class Stories extends Component {
             <tbody class="lh-copy">
               {mutableStoriesArr.map((data = JSON.parse(data), i) =>
                 <tr class="stripe-dark">
-                  <td key={JSON.parse(data).id} className='pa3'>
+                  <td key={data.id} className='pa3'>
                     <span><b>{i + 1}.</b></span>
                     <p className='para'><b>{JSON.parse(data).title} </b></p>
-                    <span><i> {JSON.parse(data).url.split('/')[2]} </i></span>
+                    <span><i>{JSON.parse(data).url ? JSON.parse(data).url.split('/')[2]: 'Error : Link Not Found'} </i></span>
+                    
                     <div>
                       <span class='score'>
                         {JSON.parse(data).score} points by {JSON.parse(data).by}
                         <div className='link'>
-                          <a href={JSON.parse(data).url} class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue">Check Story</a>
-                          <a href='#' class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-orange">Check User Posts(coming soon)</a>
+                          <a target='_blank' href={JSON.parse(data).url} class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue">Check Story</a>
+                          <span>   </span>
+                          <a href='#' class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-orange">Check User Posts</a>
                         </div>
                       </span>
                     </div>
