8e58d23b0a1862f71724b56958a79b3f08cac1f6
akash <akash619@protonmail.com>
2019-06-07 16:22:35 +0530

    added table for news

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
+import Table from './Table';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
